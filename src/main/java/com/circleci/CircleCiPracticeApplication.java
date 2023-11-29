003c118e1b50abf319d6b81bfb8f15dea410c395
Moses <moses.harvey532@gmail.com>
2023-11-29 16:31:55 +0100

    Initial commit

---
+++ src/main/java/com/circleci/CircleCiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.circleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleCiPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/circleci/CircleCiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.circleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
